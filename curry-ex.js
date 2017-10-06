d16b2650c53896fc4337a3c4c1bb54a9c64cc9bf
Umut Topuzoğlu <umut.topuzoglu@grob.de>
2017-10-06 16:53:49 +0200

    solve curry exercises

--- curry-ex.js
+++ curry-ex.js
@@ -1,59 +1,61 @@
-var _ = require('ramda');
-
+require('../../support');
+let R = require('ramda');
 
 // Exercise 1
 //==============
-// Refactor to remove all arguments by partially applying the function.
+// Refactor to remove all arguments by partially applying the function
 
-var words = function(str) {
-  return _.split(' ', str);
-};
+let words = R.split(' ');
 
 // Exercise 1a
 //==============
 // Use map to make a new words fn that works on an array of strings.
 
-var sentences = undefined;
-
+let sentences = R.map(words);
 
 // Exercise 2
 //==============
-// Refactor to remove all arguments by partially applying the functions.
+// Refactor to remove all arguments by partially applying the functions
 
-var filterQs = function(xs) {
-  return _.filter(function(x) {
-    return match(/q/i, x);
-  }, xs);
-};
+// let filterQs = function (xs) {
+//   return filter(function (x) { return match(/q/i, x); }, xs);
+// };
 
+let filterQs = R.filter(match(/q/i));
 
 // Exercise 3
 //==============
-// Use the helper function _keepHighest to refactor max to not reference any
-// arguments.
+// Use the helper function _keepHighest to refactor max to not reference any arguments
 
 // LEAVE BE:
-var _keepHighest = function(x, y) {
-  return x >= y ? x : y;
-};
+let _keepHighest = function (x, y) { return x >= y ? x : y; };
 
 // REFACTOR THIS ONE:
-var max = function(xs) {
-  return _.reduce(function(acc, x) {
-    return _keepHighest(acc, x);
-  }, -Infinity, xs);
-};
+// let max = function (xs) {
+//   return reduce(function (acc, x) {
+//     return _keepHighest(acc, x);
+//   }, -Infinity, xs);
+// };
 
+let max = R.reduce(_keepHighest, -Infinity);
 
 // Bonus 1:
 // ============
-// Wrap array's slice to be functional and curried.
-// //[1, 2, 3].slice(0, 2)
-var slice = undefined;
-
+// wrap array's slice to be functional and curried.
+// //[1,2,3].slice(0, 2)
+let slice = R.curry((start, end, xs) => xs.slice(start, end));
 
 // Bonus 2:
 // ============
 // Use slice to define a function "take" that returns n elements from the beginning of an array. Make it curried.
-// For ['a', 'b', 'c'] with n=2 it should return ['a', 'b'].
-var take = undefined;
+// For ['a', 'b', 'c'] with n=2 it should return ['a', 'b']
+let take = slice(0);
+
+module.exports = {
+  words: words,
+  sentences: sentences,
+  filterQs: filterQs,
+  max: max,
+  slice: slice,
+  take: take
+};
